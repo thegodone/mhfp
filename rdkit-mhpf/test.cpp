cbfe20d4902325481bdea3f7041563c6ca9021ef
thegodone <guillaume.godin@gmail.com>
2018-10-11 06:23:57 +0200

    adding c++ first version
    This is preliminary c++ version for testing

---
+++ rdkit-mhpf/test.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <algorithm>    // std::unique_copy, std::sort, std::distance
+#include <random>
+#include <GraphMol/MolOps.h>
+#include <GraphMol/GraphMol.h>
+#include <GraphMol/SmilesParse/SmilesParse.h>
+#include <GraphMol/SmilesParse/SmilesWrite.h>
+#include <GraphMol/Subgraphs/Subgraphs.h>
+#include <GraphMol/Subgraphs/SubgraphUtils.h>
+#include "sha1.hpp"
+
+
+using namespace RDKit;
+struct compare
+{
+    int key;
+    compare(int const &i): key(i) { }
+
+    bool operator()(int const &i)
+    {
+        return (i == key);
+    }
+};
+
+
+struct distrib{
+	std::vector<uint64_t> da;
+	std::vector<uint64_t> db;
+	long int max_hash;
+	long long int prime;
+};
+
+
+// generator of distribution for Hashing
+distrib generatordistrib(int seed, int distribsize){
+
+	distrib dab;
+	long int max_hash = std::llround(std::pow(2,32)); // 4294967295 +1 ? ...
+    long long int prime = std::llround(std::pow(2,61)); // 2305843009213693951 +1 ? ...
+ 	std::vector<uint64_t> v1;
+ 	std::vector<uint64_t> v2;
+
+	// generator 1:
+    std::mt19937::result_type myseed = seed;
+    std::mt19937 e1(myseed); //_64 to have 64 bits double not needed
+    std::uniform_int_distribution<long long int> dist1(std::llround(0.0), std::llround(std::pow(2,32)));
+    while (v1.size()<distribsize) {
+    	long long int a = dist1(e1);
+        bool result = !std::any_of(v1.begin(),v1.end(),compare(a));
+        if (result) {
+        v1.push_back(a);
+
+    	}
+    }
+
+    // generator 2:
+    std::mt19937 e2(myseed); //_64 to have 64 bits double not needed
+    std::uniform_int_distribution<long long int> dist2(std::llround(1.0), std::llround(std::pow(2,32)));
+    while (v2.size()<distribsize) {
+    	long long int b = dist2(e2);
+        bool result = !std::any_of(v2.begin(),v2.end(),compare(b));
+        if (result) {
+        v2.push_back(b);
+    	}
+    }
+
+    dab.da=v1;
+    dab.db=v2;
+    dab.prime=prime;
+    dab.max_hash=max_hash;
+    return dab;
+
+
+}
+
+
+std::vector<std::string> shingling_from_mol(RDKit::ROMol *in_mol, int radius, bool rings, bool kekulize) {
+    std::vector<std::string> shingling;
+    if (rings){
+    	PATH_TYPE bonds;
+		RDKit::VECT_INT_VECT molrings;
+  		RDKit::MolOps::symmetrizeSSSR(*in_mol, molrings);
+  		for (const auto molring : molrings) {
+  			for (const auto index1: molring) {
+  				//std::cout << "index1 : "<< index1 << std::endl;
+      			for (const auto index2: molring) {
+      				//std::cout << "index2 : "<< index2 << std::endl;
+      				if(index2!=index1) {
+      					const Bond *bnd = in_mol->getBondBetweenAtoms(index1, index2);
+      					if (bnd) {
+      						bool result = !std::any_of(bonds.begin(),bonds.end(),compare(bnd->getIdx()));
+      						if (result) {
+      							bonds.push_back(bnd->getIdx());
+      						}
+                       	}
+                    }
+                }
+    		}
+  		}
+
+
+  		// by default canonical MolToSmiles
+  		RDKit::ROMol *submol = Subgraphs::pathToSubmol(*in_mol, bonds);
+  		std::string subsmi = RDKit::MolToSmiles(*submol);
+
+        shingling.push_back(subsmi);
+	}
+
+
+
+	PATH_TYPE p;
+	std::string smiles;
+    for (int index = 0; index < in_mol->getNumAtoms(); index++){
+      for (int i = 1; i < radius + 1;  i++){
+      	p = RDKit::findAtomEnvironmentOfRadiusN(*in_mol, i, index);
+        INT_MAP_INT amap; // map
+        ROMol *submol = Subgraphs::pathToSubmol(*in_mol, p, false, amap);
+        // check if the atomindex match the current index value!
+        if (amap.count(index) > 0){
+	        smiles = RDKit::MolToSmiles(*submol, true, false, amap[index], true);
+	        if (!smiles.empty()){
+	           shingling.push_back(smiles);
+	        }
+        }
+      }
+	}
+	// remove duplicates then!
+	sort (shingling.begin(), shingling.end());
+    shingling.erase(std::unique(shingling.begin(), shingling.end()), shingling.end());
+    return shingling;
+}
+
+
+std::vector<uint32_t> from_molecular_shingling(std::vector<std::string> token, distrib dab, int distribsize){
+	// set the hash_values
+	std::vector<uint32_t> hash_values;
+
+	for (int i=0;i<distribsize;i++){
+		hash_values.push_back(dab.max_hash);
+	}
+	// generate hashes
+	for(const auto res : token){
+		SHA1 checksum;
+    	checksum.update(res);
+    	uint32_t t_h = checksum.Hash();
+    	for (int j=0;j<distribsize;j++){
+			hash_values[j] = fmin( hash_values[j] , remainder( remainder( dab.da[j] * t_h + dab.db[j], dab.prime) , dab.max_hash) );
+		}
+	}
+	return hash_values;
+}
+
+
+
+int main()
+{
+
+	int bitsize = 120;
+    distrib dab = generatordistrib(42, bitsize);
+    std::cout << dab.da.size()<< ", " << dab.db.size()<< std::endl ;
+	//std::cout << "prime : "<< dab.prime << std::endl;
+	//std::cout << "maxhash : "<< dab.max_hash << std::endl;
+
+	RDKit::ROMol *mol1 = RDKit::SmilesToMol("c1ccccc1COCC");
+
+	std::vector<std::string> res = shingling_from_mol(mol1, 3, true, true);
+
+    std::vector<uint32_t> hash_values = from_molecular_shingling(res, dab, bitsize);
+	for (int i=0;i<bitsize;i++){
+		std::cout <<  hash_values[i]  << ",";
+	}
+
+	std::cout << std::endl;
+
+}
+
---
+++ rdkit-mhpf/test.py
@@ -0,0 +1,106 @@
+import numpy as np
+from rdkit.Chem import AllChem
+from hashlib import sha1
+import struct
+
+
+prime = (1 << 61) - 1
+max_hash = (1 << 32) - 1
+
+print(prime)
+print(max_hash)
+
+def gen(n_permutations = 2048, seed = 42):
+    """All minhashes created using this instance will use the arguments
+    supplied to the constructor.
+
+    Keyword arguments:
+        n_permutations {int} -- The number of permutations used for hashing (default: {128})
+        seed {int} -- The value used to seed numpy.random (default: {42})
+    """  
+
+    permutations_a = np.zeros([n_permutations], dtype=np.uint32)
+    permutations_b = np.zeros([n_permutations], dtype=np.uint32)
+
+    #print(permutations_a)
+    #print(permutations_b)
+
+    permutations_a_64 = np.zeros([n_permutations], dtype=np.uint64)
+    permutations_b_64 = np.zeros([n_permutations], dtype=np.uint64)
+
+    rand = np.random.RandomState(seed)
+
+    # This is done in a loop as there shouldn't be any duplicate random numbers.
+    # Also, numpy.random.choice seems to be implemented badly, as it throws
+    # a memory error when supplied with a large n.
+    for i in range(n_permutations):
+      a = rand.randint(1, max_hash, dtype=np.uint32)
+      #print(a)
+      b = rand.randint(0, max_hash, dtype=np.uint32)
+
+      while a in permutations_a:
+        a = rand.randint(1, max_hash, dtype=np.uint32)
+        #print("whilea:")
+        #print(a)
+      
+      while b in permutations_b:
+        b = rand.randint(0, max_hash, dtype=np.uint32)
+        #print("whileb:")
+        #print(b)
+      
+      permutations_a[i] = a
+      permutations_b[i] = b
+
+
+    # Reshape into column vectors
+    permutations_a = permutations_a.reshape((n_permutations, 1))
+    permutations_b = permutations_b.reshape((n_permutations, 1))
+    return permutations_a, permutations_b
+
+
+def shingling_from_mol(in_mol, radius=3, rings=True, kekulize=True):
+    shingling = []
+    if rings:
+      for ring in AllChem.GetSymmSSSR(in_mol):
+        bonds = set()
+        ring = list(ring)
+        for i in ring:
+          for j in ring:
+            if i != j:
+              bond = in_mol.GetBondBetweenAtoms(i, j)
+              print(bond)
+              if bond != None:
+                bonds.add(bond.GetIdx())
+        shingling.append(AllChem.MolToSmiles(AllChem.PathToSubmol(in_mol, list(bonds)), canonical=True).encode('utf-8'))
+
+    for index, _ in enumerate(in_mol.GetAtoms()):
+      for i in range(1, radius + 1):
+        p = AllChem.FindAtomEnvironmentOfRadiusN(in_mol, i, index)
+        amap = {}
+        submol = AllChem.PathToSubmol(in_mol, p, atomMap=amap)
+        if index not in amap:
+          continue
+        smiles = AllChem.MolToSmiles(submol, rootedAtAtom=amap[index], canonical=True)
+        if smiles is not '':
+          shingling.append(smiles.encode('utf-8'))
+
+    # Set ensures that the same shingle is not hashed multiple times
+    # (which would not change the hash, since there would be no new minima)
+    return list(set(shingling))
+
+
+mol = AllChem.MolFromSmiles("c1ccccc1");
+
+res = shingling_from_mol(mol, 3);
+permutations_a,permutations_b =gen()
+hash_values = np.zeros([2048, 1], dtype=np.uint32)
+hash_values.fill(max_hash)
+
+for t in res:
+    print(sha1(t).digest()[:4])
+    t_h = struct.unpack('<I', sha1(t).digest()[:4])[0]
+    print(t_h)
+    hashes = np.remainder(np.remainder(permutations_a * t_h + permutations_b, prime), max_hash)
+    hash_values = np.minimum(hash_values, hashes)
+
+print(hash_values.reshape((1, 2048))[0])
